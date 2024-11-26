5d6f115d2f1fe5e6e3b9134035b1b74595ab3904
Santosh Bhattarai <santoshbhattarai287@gmail.com>
2024-11-26 16:54:55 +0545

    cards created

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,62 +2,113 @@ import { useEffect, useState } from "react";
 import PokemonCards from "./PokemonCards";
 
 export default function Cards() {
+  const [pokemon, setPokemon] = useState([]);
+  const [loaading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
 
-    const [pokemon, setPokemon] = useState([])
-    const [loaading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
 
-    const API= 'https://pokeapi.co/api/v2/pokemon?limit=100'
+  const fetchAPI = async () => {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+      const detailedPokemonData = await Promise.all(
+        data.results.map(async (currPokemon) => {
+          const response = await fetch(currPokemon.url);
+          const data = await response.json();
 
-   
-
-    const fetchAPI= async () => {
-        try{
-            const response= await fetch(API)
-            const data= await response.json()
-            const detailedPokemonData= await Promise.all(
-            data.results.map(async (currPokemon)=>{
-                const response= await fetch(currPokemon.url)
-                const data= await response.json()
-                
-                return data;
-            }));
-            setPokemon(detailedPokemonData);
-            const detailedResponse=Promise.all(detailedPokemonData)
-            console.log(detailedResponse)
-            setLoading(false)
-        }
-        catch(error){
-            console.log(error)
-            setLoading(false)
-            setError(error)
-        }
-        
+          return data;
+        })
+      );
+      setPokemon(detailedPokemonData);
+      const detailedResponse = Promise.all(detailedPokemonData);
+      console.log(detailedResponse);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
     }
+  };
 
-    useEffect(() => {
-        fetchAPI()
-    }, [])
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
-    if(loaading){
-        return <h1 className="text-2xl font-bold flex justify-center">Loading...</h1>
-    }
+  // search functionility
+  const searchData = pokemon.filter((currPokemon) => {
+    return currPokemon.name.toLowerCase().includes(search.toLowerCase());
+  });
 
-    if(error){
-        return <h1 className="text-2xl font-bold flex justify-center">Error: {error.message}</h1>
-    }
+  if (loaading) {
+    return (
+      <h1 className="text-2xl font-bold flex justify-center">Loading...</h1>
+    );
+  }
+
+  if (error) {
+    return (
+      <h1 className="text-2xl font-bold flex justify-center">
+        Error: {error.message}
+      </h1>
+    );
+  }
   return (
     <>
-    <section className="container mx-auto">
-                <header>
-                    <h1 className="text-center text-4xl font-bold mt-5">Pokemon Cards</h1>
-                </header>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {pokemon.map((currPokemon) => {
-                        return <PokemonCards key={currPokemon.id} currPokemon={currPokemon} />
-                    })}
-                </div>
-            </section>
+      <section className="container mx-auto">
+        <header>
+          <h1 className="text-center text-4xl font-bold mt-5 mb-3">
+            Pokemon Cards
+          </h1>
+        </header>
+
+        <form class="max-w-md mx-auto">
+          <label
+            for="default-search"
+            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search Mockups, Logos..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              autoComplete="off"
+              required
+            />
+          </div>
+        </form>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {searchData.map((currPokemon) => {
+            return (
+              <PokemonCards key={currPokemon.id} currPokemon={currPokemon} />
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
--- src/Components/PokemonCards.jsx
+++ src/Components/PokemonCards.jsx
@@ -1,25 +1,25 @@
-
-const PokemonCards =({currPokemon})=>{
-    return(
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden mt-10">
-            <img src={currPokemon.sprites.front_default} alt={currPokemon.name} className="w-full h-48 object-cover" />
-            <div className="p-4">
-                <div className="flex justify-between items-center bg-green-500 pl-24 p-1 rounded-md">
-                    <p>
-                        {currPokemon.types.map((type) => type.type.name).join(", ")}
-                    </p>
-                </div>
-                
-                <h2 className="text-xl font-bold mb-2">{currPokemon.name}</h2>
-                <p className="text-gray-700">Type: {currPokemon.types.map((type) => type.type.name).join(", ")}</p>
-                <p className="text-gray-700">Height: {currPokemon.height}</p>
-                <p className="text-gray-700">Weight: {currPokemon.weight}</p>
-                <p className="text-gray-700">Abilities: {currPokemon.abilities.map((ability) => ability.ability.name).join(", ")}</p>
-                
-            </div>
-            
+const PokemonCards = ({ currPokemon }) => {
+    return (
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden mt-10 grid grid-cols-1">
+        <img
+          src={currPokemon.sprites.front_default}
+          alt={currPokemon.name}
+          className="w-full h-48 object-cover"
+        />
+        <div className="p-4">
+          <div className="flex justify-center text-center bg-green-500 px-4 py-1 rounded-md">
+            <p className="text-white text-sm flex-grow">
+              {currPokemon.types.map((type) => type.type.name).join(", ")}
+            </p>
+          </div>
+          <h2 className="text-xl font-bold mb-2">{currPokemon.name}</h2>
+          <p className="text-gray-700">Type: {currPokemon.types.map((type) => type.type.name).join(", ")}</p>
+          <p className="text-gray-700">Height: {currPokemon.height}</p>
+          <p className="text-gray-700">Weight: {currPokemon.weight}</p>
+          <p className="text-gray-700">Abilities: {currPokemon.abilities.map((ability) => ability.ability.name).join(", ")}</p>
         </div>
-    )
-}
-
-export default PokemonCards
+      </div>
+    );
+  };
+  
+  export default PokemonCards;
